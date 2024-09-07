a41476279f3603b244322fbc78573c267a6d42d7
Ji-Yoon Lee <dlwldbs369@tukorea.ac.kr>
2024-09-07 18:57:24 +0900

    Initial settings
    초기 세팅

---
+++ dalda/src/test/java/com/project/dalda/DaldaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dalda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
