0ef2391e912b3e485056fe1cde95c0838d132c6b
지윤 <dlwldbs369@tukorea.ac.kr>
2024-10-27 18:07:56 +0900

    [fix] 충돌 방지를 위해 파일 삭제

--- dalda/src/main/java/com/project/dalda/alcohol/repository/TraditionalLiquorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.dalda.alcohol.repository;
-
-import com.project.dalda.alcohol.entity.TraditionalLiquor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TraditionalLiquorRepository extends JpaRepository<TraditionalLiquor, Long> {
-    List<TraditionalLiquor> findByIdIn(List<Long> ids);
-}
