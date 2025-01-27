9f92189fe24d50df55fefa1d7ab67cc7a3efed4e
지윤 <dlwldbs369@tukorea.ac.kr>
2025-01-27 17:11:56 +0900

    [Feat/#12] Redis 도입 및 설정 파일 수정
    - redis 도입으로 전체 검색 속도 개선
    - 환경 변수 외부 주입

---
+++ dalda/src/main/java/com/project/dalda/search/config/RedisDataLoader.java
@@ -0,0 +1,30 @@
+package com.project.dalda.search.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.dalda.alcohol.service.AlcoholService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RedisDataLoader {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final AlcoholService alcoholService;
+
+    public void loadData() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Object soju = objectMapper.convertValue(alcoholService.getSojuAlcohols(), Object.class);
+        Object beer = objectMapper.convertValue(alcoholService.getBeerAlcohols(), Object.class);
+        Object sake = objectMapper.convertValue(alcoholService.getSakeAlcohols(), Object.class);
+        Object traditionalLiquor = objectMapper.convertValue(alcoholService.getTraditionalLiquorAlcohols(), Object.class);
+        Object wine = objectMapper.convertValue(alcoholService.getWineAlcohols(), Object.class);
+        Object whisky = objectMapper.convertValue(alcoholService.getWiskyAlcohols(), Object.class);
+        redisTemplate.opsForValue().set("soju", soju);
+        redisTemplate.opsForValue().set("beer", beer);
+        redisTemplate.opsForValue().set("sake", sake);
+        redisTemplate.opsForValue().set("traditionalLiquor", traditionalLiquor);
+        redisTemplate.opsForValue().set("wine", wine);
+        redisTemplate.opsForValue().set("whisky", whisky);
+    }
+}
